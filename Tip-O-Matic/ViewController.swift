152a1070e75645dc69ac9f120740b901b56e66bb
Cesar Cavazos <me@ccavazos.co>
2017-08-22 21:38:29 -0700

    Initial Commit

---
+++ Tip-O-Matic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip-O-Matic
+//
+//  Created by Cesar Cavazos on 8/22/17.
+//  Copyright © 2017 Cesar Cavazos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tip-O-MaticTests/Tip_O_MaticTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tip_O_MaticTests.swift
+//  Tip-O-MaticTests
+//
+//  Created by Cesar Cavazos on 8/22/17.
+//  Copyright © 2017 Cesar Cavazos. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_O_Matic
+
+class Tip_O_MaticTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
